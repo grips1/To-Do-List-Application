7e8fb8ed36b84c65b0174ae8d5aeeab65a57283d
fendermcbender <shayke.gols@gmail.com>
2022-12-19 16:58:06 +0300

    Demo V1
    Input task function(with user input) and print list function.
    Next goal is to make the program a menu, rather than edit-based.
    (And get rid of the BOs lolol)

--- To Do App/main.c
+++ To Do App/main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-
-int PrintTasks(char* filename)
+int PrintTasks()
 {
     char c;
     int lines = 0;
     FILE* fp;
-    fp = fopen(filename, "r");
+    fp = fopen("TaskList.txt", "r");
     while(1)
     {
         c = fgetc(fp);
@@ -16,12 +15,29 @@ int PrintTasks(char* filename)
         printf("%c", c);
     }
     fclose(fp);
-    printf("\n\nPrinted task list with %d tasks.\n", lines);
+    printf("\nPrinted task list with %d tasks.\n", lines);
     return lines;
 }
-
+void AddTask(char Task[])
+{
+    FILE* fp;
+    fp = fopen("TaskList.txt", "a");
+    fputs(Task, fp);
+    fputs(".\n", fp);
+    fclose(fp);
+}
 int main()
 {
+    char str[60];
+    int ch;printf("\n %s", str);
+    AddTask(str);
+    PrintTasks("TaskList.txt");printf("\n %s", str);
+    AddTask(str);
     PrintTasks("TaskList.txt");
+    PrintTasks();
+    printf("Enter your task: ");
+    gets(str);
+    AddTask(str);
+    PrintTasks();
     return 0;
 }

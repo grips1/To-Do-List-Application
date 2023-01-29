ce81ebcf1c4e722ce204dbad3856f2c635cb5b23
fendermcbender <shayke.gols@gmail.com>
2023-01-29 21:01:45 +0300

    Reverted
    I ended up causing more issues than fixing any, so I'm reverting back to the latest "working" version until I find a solution.

--- To Do App/main.c
+++ To Do App/main.c
@@ -5,17 +5,16 @@
 int main()
 {
     printf("Welcome.\nSelect a number to continue:\n");
-    int c;
+    char c;
     while(1)
     {
-        printf("1)Print current list.\n2)Add a task to the list.\n3)Exit.\nInput:\n>");
-        scanf("%d[^\n]", &c);
-        switch(c)
+        if(!(c == '\n')) printf("1)Print current list.\n2)Add a task to the list.\n3)Exit.\nInput:");
+        switch(c = getchar())
         {
-            case 1: PrintTasks(); break;
-            case 2: AddTask(); break;
-            case 3: printf("\nQuitting...\nGOODBYE!\n"); goto exit; break;
-            default: printf("\nINVALID! Try again.\n\n"); break;
+            case '1': PrintTasks(); break;
+            case '2': AddTask(); break;
+            case '3': printf("Quitting...\nGoodbye."); goto exit;
+            default: printf("\nInvalid input, try again.\n");
         }
     }
     exit: return 0;
@@ -35,16 +34,17 @@ void PrintTasks() // Works
         c = fgetc(fp);
     }
     fclose(fp);
-    printf("\nPrinted task list with %d tasks.\n>Returning to menu...\n", lines);
+    printf("\nPrinted task list with %d tasks.\nReturning to menu.\n", lines);
 }
 void AddTask()
 {
     char str[60];
     int ch;
     FILE* fp = fopen("TaskList.txt", "a"); // opens file for appending;
-    printf("\nEnter task(line break to finish):\n>");
-    scanf("%59s", str);
-    fputs(&str, fp);
+    for(int index = 0; (ch = getchar()) != '.'; index++)
+    {
+        fputs(ch, fp);
+    }
     fputc("\n", fp);
     fclose(fp);
 }

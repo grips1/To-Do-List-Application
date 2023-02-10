4c3b4f4d73908bfd7973d0596f9e0b3b2634b6fd
fendermcbender <shayke.gols@gmail.com>
2023-02-10 16:43:31 +0300

    Task input's buggy asf
    cuz of scanf("\n")

--- To Do App/main.c
+++ To Do App/main.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-
-int main()
-{
-    printf("Welcome.\nSelect a number to continue:\n");
-    char c;
-    while(1)
-    {
-        if(!(c == '\n')) printf("1)Print current list.\n2)Add a task to the list.\n3)Exit.\nInput:");
-        switch(c = getchar())
-        {
-            case '1': PrintTasks(); break;
-            case '2': AddTask(); break;
-            case '3': printf("Quitting...\nGoodbye."); goto exit;
-            default: printf("\nInvalid input, try again.\n");
-        }
-    }
-    exit: return 0;
-}
+#include <strings.h>
 
 void PrintTasks() // Works
 {
@@ -26,25 +9,44 @@ void PrintTasks() // Works
     int lines = 0;
     FILE* fp;
     fp = fopen("TaskList.txt", "r");
-    for(c = fgetc(fp); !(feof(fp));)
+    while(c = fgetc(fp))
     {
         if(c == '\n') lines++;
         if(feof(fp)) break;
         printf("%c", c);
-        c = fgetc(fp);
     }
     fclose(fp);
     printf("\nPrinted task list with %d tasks.\nReturning to menu.\n", lines);
 }
+
 void AddTask()
 {
+    scanf("\n");
     char str[60];
-    int ch;
-    FILE* fp = fopen("TaskList.txt", "a"); // opens file for appending;
-    for(int index = 0; (ch = getchar()) != '.'; index++)
-    {
-        fputs(ch, fp);
-    }
-    fputc("\n", fp);
+    scanf("%[^\n]%*s", str);
+    FILE* fp = fopen("TaskList.txt", "a"); // opens file for appending
+    fputs(str, fp);
+    fputc('\n', fp);
     fclose(fp);
 }
+
+int main()
+{
+    printf("Welcome.\nSelect a number to continue:\n");
+    char c;
+    while(1)
+    {
+        printf("1)Print current list.\n2)Add a task to the list.\n3)Exit.\nInput:");
+        switch(c = getchar())
+        {
+            case '1': PrintTasks(); break;
+            case '2': AddTask(); break;
+            case '3': printf("Quitting...\nGoodbye."); goto exit;
+            default: printf("\nInvalid input, try again.\n");
+        }
+        scanf("\n");
+    }
+    exit: return 0;
+}
+
+
